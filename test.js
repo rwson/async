4e4cc1937082baeeff46bd3ff380e9d82d6924e8
rwson <rw_Song@sina.com>
2017-06-14 17:32:56 +0800

    add each

--- test.js
+++ test.js
@@ -1,21 +1,51 @@
-const waterfall = require("./lib/waterfall");
+const fs = require("fs");
+const async = require("async");
+const request = require("request");
 
-waterfall([
-	function(cb) {
-		console.log(new Date);
-		setTimeout(function() {
-			cb(null, 123);
-		}, 2000);
-	},
-	function(arg, cb) {
-		console.log(new Date);
-		setTimeout(function() {
-			console.log(arg);
-			cb(null, 123, 456);
-		}, 2000);
-	},
-	function(arg1, arg2, cb) {
-		console.log(new Date);
-		console.log(arg1, arg2);
-	}
-], function() {});
+const sites = ['www.baidu.com','github.com','www.npmjs.com', 'www.zhihu.com'];
+
+const async2 = require("./lib");
+
+// async2.waterfall([
+//     function (cb) {
+//         console.log(new Date);
+//         setTimeout(function () {
+//             cb(null, 123);
+//         }, 2000);
+//     },
+//     function (arg, cb) {
+//         console.log(new Date);
+//         setTimeout(function () {
+//             console.log(arg);
+//             cb(null, 123, 456);
+//         }, 2000);
+//     },
+//     function (arg1, arg2, cb) {
+//         console.log(new Date);
+//         console.log(arg1, arg2);
+//     }
+// ], function (ex) {
+//     if (ex) {
+//         throw ex;
+//     }
+// });
+
+function downloadFavicon(site, next) {
+    let addr = `https://${site}/favicon.ico`;
+    let file = `./${site}.ico`;
+    request.get(addr)
+        .pipe(fs.createWriteStream(file))
+        .on('error', (err) => {
+            console.error(`${url} Download failed: ${err.message}`);
+            next();
+        })
+        .on('finish', next);
+}
+
+async2.each(sites, downloadFavicon, function (err) {
+    if (err) {
+        console.log('err', err);
+    }
+    console.log('over');
+});
+
